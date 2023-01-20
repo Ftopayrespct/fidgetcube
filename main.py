2fe8f2b0c7ba3076630ad8af392de66cc337cc8c
Ftopayrespct <122543938+Ftopayrespct@users.noreply.github.com>
2023-01-20 09:53:36 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,143 +1,24 @@
-def on_button_pressed_a():
-    basic.show_icon(IconNames.HAPPY)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_gesture_tilt_left():
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . # . .
-    """)
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . # . . .
-    """)
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .
-    """)
-input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
-
-def on_button_pressed_ab():
-    basic.show_leds("""
-        . # # # .
-                . # . # .
-                . . # # .
-                . . . . .
-                . . # . .
-    """)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . # . .
-    """)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_gesture_shake():
-    while True:
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        . . # . .
-        """)
-        basic.show_leds("""
-            . . . # .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-        """)
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . #
-        """)
-        basic.show_leds("""
-            . . # . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-        """)
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        # . . . .
-                        . . . . .
-                        . . . . .
-        """)
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . # . . .
-                        . . . . .
-                        . . . . .
-        """)
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        . # . . .
-                        . . . . .
-        """)
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-
-def on_gesture_tilt_right():
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . # . .
-    """)
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . # .
-    """)
-    basic.show_leds("""
-        . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . #
-    """)
-input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
-
-basic.show_leds("""
-    # . . . #
-        # . . . #
-        # # # # #
-        # . . . #
-        # . . . #
-""")
-basic.show_leds("""
-    . . # . .
-        . . . . .
-        . . # . .
-        . . # . .
-        . . # . .
-""")
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+let RPS = 0
+input.onButtonPressed(Button.A, function () {
+    RPS += randint(1, 3)
+    if (RPS == 1) {
+        basic.showString("R")
+    } else if (RPS == 2) {
+        basic.showString("P")
+    } else {
+        basic.showString("S")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    RPS += randint(1, 3)
+    if (RPS == 1) {
+        basic.showString("R")
+    } else if (RPS == 2) {
+        basic.showString("P")
+    } else {
+        basic.showString("S")
+    }
+})
+basic.forever(function () {
+	
+})
